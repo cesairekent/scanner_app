59040dbd4c5e60874390e695f016f24c73130d38
Mini Titan <fokoemmanuel@gmail.com>
2022-04-08 19:51:51 +0100

    add data

---
+++ lib/src/views/qr_code_scanner_view.dart
@@ -0,0 +1,121 @@
+import 'dart:developer';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+
+class QRScannerView extends StatefulWidget
+{
+  @override
+  _QRScannerViewState createState() => new _QRScannerViewState();
+}
+
+class _QRScannerViewState extends State<QRScannerView> {
+  Barcode? result;
+  QRViewController? controller;
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+
+  // In order to get hot reload to work we need to pause the camera if the platform
+  // is android, or resume the camera if the platform is iOS.
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller!.pauseCamera();
+    }
+    controller!.resumeCamera();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Expanded(flex: 1, child: _buildQrView(context)),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildQrView(BuildContext context) {
+    // For this example we check how width or tall the device is and change the scanArea and overlay accordingly.
+    var scanArea = (MediaQuery.of(context).size.width < 400 ||
+            MediaQuery.of(context).size.height < 400)
+        ? 200.0
+        : 400.0;
+    // To ensure the Scanner view is properly sizes after rotation
+    // we need to listen for Flutter SizeChanged notification and update controller
+    return QRView(
+      key: qrKey,
+      onQRViewCreated: _onQRViewCreated,
+      overlay: QrScannerOverlayShape(
+          borderColor: Colors.yellowAccent,
+          borderRadius: 10,
+          borderLength: 30,
+          borderWidth: 5,
+          cutOutSize: scanArea),
+      onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    setState(() {
+      this.controller = controller;
+    });
+
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData;
+      });
+      doAfterScan(context);
+    });
+  }
+
+  void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
+    log('${DateTime.now().toIso8601String()}_onPermissionSet $p');
+    if (!p) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('no Permission')),
+      );
+    }
+  }
+
+  doAfterScan(BuildContext context) async {
+    controller?.pauseCamera();
+    HapticFeedback.mediumImpact();
+
+    var scanData = result!.code.toString();
+
+
+  }
+
+  qrCodeInvalid(BuildContext context) {
+    return Alert(
+      context: context,
+      type: AlertType.error,
+      title: 'erreur',
+      desc: 'Le code QR est invalid',
+      style: AlertStyle(alertElevation: 10, backgroundColor: Colors.white10),
+      buttons: [
+        DialogButton(
+            color: Colors.blue,
+            child: const Text(
+              'OK',
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+              controller?.resumeCamera();
+            })
+      ],
+    ).show();
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+}
